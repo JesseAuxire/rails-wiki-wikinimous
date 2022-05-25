539f600bfa09ebb514babc0f78ffb55022dc35e1
Jesse Auxire <auxire.jesse@hotmail.fr>
2022-05-25 15:48:51 +0200

    articles in index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do 
+  Article.create(
+    title: Faker::Company.catch_phrase,
+    content: Faker::Company.catch_phrase
+  )
+end
